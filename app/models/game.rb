387e065f4c5fa2c298e47b7e06dbd7fd404b3a05
Benjamin Baranger <baranger.benjamin@gmail.com>
2022-01-15 16:31:42 +0000

    shuffle words on creation + add next/previous record methods

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ApplicationRecord
   has_many :attempts
+
+  def next
+    Game.where("id > #{self.id}").first
+  end
+
+  def previous
+    Game.where("id < #{self.id}").first
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ puts "------------------"
 puts "------------------"
 puts "------------------"
 puts "Creating all games"
-File.open("words_size_5.txt").read.split("\n").each_with_index do |word, index|
+File.open("words_size_5.txt").read.split("\n").shuffle.each_with_index do |word, index|
     Game.create(word: word)
     puts "created #{index + 1} word(s)"
 end
