9e1d248fc604b2b3deaef8e6f223f247a033140a
Alex Haumer <alexander.haumer@me.com>
2022-01-15 14:03:32 +0000

    Color coded attempt letters

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,7 +1,7 @@
-class AttemptsController < ApplicationController  
+class AttemptsController < ApplicationController
     def update
-        @attempt = Attempt.find(params[:id])
-        @attempt.update(words: params[:attempt][:words].split(","))
+      @attempt = Attempt.find(params[:id])
+      @attempt.update(words: params[:attempt][:words].split(","))
+      redirect_to game_path(@attempt.game)
     end
   end
-  
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def letter_background(letter, index, word)
+    return 'background-color: #6aaa64' if word[index] == letter
+
+    return 'background-color: #ffe156' if word.include?(letter) && letter != ''
+
+    return 'background-color: #3a3a3c'
+  end
 end
