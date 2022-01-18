e7ba971cce2990d125dec3ac1d01074d5655f114
Alex Haumer <alexander.haumer@me.com>
2022-01-18 00:37:58 +0000

    Reworked profile a little

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @unsolved = @user.attempts.where(finished: false)
+      .where.not(solved: true).select { |attempt| attempt.words.any? }.first(3)
   end
 end
